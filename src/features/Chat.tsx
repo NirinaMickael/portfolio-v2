a6808be870edae00f5530326f078bea60b1bbe04
NirinaMickael <todisoanirinamickael@gamil.com>
2022-06-08 10:55:08 +0300

    contact

--- src/features/Chat.tsx
+++ src/features/Chat.tsx
@@ -1,14 +1,25 @@
 import { motion, useAnimation } from "framer-motion";
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm , SubmitHandler } from "react-hook-form";
 import { Close } from "@mui/icons-material";
 import { Chat } from "../@core/data/variant";
+interface IFormInput {
+  message : string;
+}
 const Chats = React.memo(({close} :{close : any}) => {
-    return (
+  const {register , handleSubmit , formState : {isValid}}  = useForm<IFormInput>();
+  const onSubmit  : SubmitHandler<IFormInput> = data => console.log(data);
+  useEffect(()=>{
+    if(isValid){
+      console.log("ok")
+    }
+  },[isValid])  
+  return (
     <motion.div 
     variants={Chat}
     initial="hidden"
     animate="visible"
-    className="chat">
+    className="chat grid">
       <div className="headerChat w-full border-b-2 border-black">
         <h3 className=" user m-1">You</h3>
         <Close
@@ -18,8 +29,17 @@ const Chats = React.memo(({close} :{close : any}) => {
           sx={{ fontSize: 25 }}
         />
       </div>
-      <div className="wrapperform w-full">
-
+      <div className="wrapperChat">
+        <p>Kiku</p>
+          
+      </div>
+      <div className="wrapperform px-1">
+        <form onSubmit={handleSubmit(onSubmit)}  >
+          <input  {...register("message", { required: true, minLength: 5 })} 
+          className="w-56 py-2 outline-none px-2 text-xs" placeholder="typing......." />
+          <button type="submit" disabled={!isValid}
+          className="w-2O bg-blue-500 hover:bg-blue-400 text-white  py-1 px-3 border-b-2 border-blue-700 hover:border-blue-500 rounded"> Send</button>
+        </form>
       </div>
     </motion.div>
   );
